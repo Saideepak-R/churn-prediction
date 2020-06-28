5a9776c380b08316222aedefe4fd55b62fb29dab
Saideepak-R <45707011+Saideepak-R@users.noreply.github.com>
2020-06-28 22:02:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 from pycaret.classification import load_model, predict_model
 import streamlit as st
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+import pandas as pd
+
 
 model = load_model('xgboost_for_deployment')
-image = Image.open('logo.png')
-image_digital_trust = Image.open('digital_trust.jpg')
+
 
 def run():
 	from PIL import Image
+	image = Image.open('logo.png')
+	image_digital_trust = Image.open('digital_trust.jpg')
 	st.image(image , use_column_width = False)
-#st.sidebar.image(image  , use_column_width = False)
-    
-#st.sidebar.info('ROC insights')
-#st.sidebar.success('https://www.subex.com/act/insights/')
-#st.sidebar.image(image_digital_trust)
 	st.title('Telecom Churn Prediction App')
 
 	file_upload = st.file_uploader('Upload csv file for predictions', type = ['csv'])
@@ -24,8 +21,6 @@ def run():
 	if file_upload is not None:
 		data = pd.read_csv(file_upload)
 		predictions = predict_model(estimator = model , data = data)
-	#predictions1 = predictions['Label'].map(lambda x : 'Not churn' if x == 0 else 'Churn' )
-	#st.write(predictions)
 		predictions.Label[predictions.Label  ==  0 ] = 'Non churn'
 		predictions.Label[predictions.Label  ==  1 ] = 'Churn'
 		g = sns.countplot(x = 'Label' , data = predictions )
