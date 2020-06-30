729e15e05c1e14ccbbf0bdaa73fcb7944d07a300
Saideepak-R <45707011+Saideepak-R@users.noreply.github.com>
2020-06-30 18:41:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def run():
 
     if file_upload is not None:
         data = pd.read_csv(file_upload)
-	data_new = pd.merge(data , data1 , how = 'inner' , on = 'phone number')
+        data_new = pd.merge(data , data1 , how = 'inner' , on = 'phone number')
         predictions = predict_model(estimator=model,data=data_new)
         predictions.Label[predictions.Label  ==  0 ] = 'Non churn'
         predictions.Label[predictions.Label  ==  1 ] = 'Churn'
@@ -32,4 +32,4 @@ def run():
     st.pyplot()
 
 if __name__ == '__main__':
-    run()	
+    run()
